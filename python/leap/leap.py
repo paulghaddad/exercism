4afce7c25b8e64f5bdd77b179c044e00ff0ba617
Paul Haddad <paulh16@gmail.com>
2018-10-09 16:08:00 -0400

    Iteration 3

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -6,10 +6,7 @@ def is_leap_year(year):
     """Return True if the year is a leap year"""
 
     if year % 4 == 0:
-        if not year % 100 == 0:
-            return True
-
-        if year % 100 == 0 and year % 400 == 0:
+        if year % 100 != 0 or year % 400 == 0:
             return True
 
     return False
