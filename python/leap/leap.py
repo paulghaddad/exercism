f0bee1cf19f465cc8f7a87371f8b37dd1e0c72b4
Paul Haddad <paulh16@gmail.com>
2018-10-12 07:41:14 -0400

    Leap.py
    * Iteration 5

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -5,7 +5,4 @@ Exercism Problem 2: Leap
 def is_leap_year(year):
     """Return True if the year is a leap year"""
 
-    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-        return True
-
-    return False
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
