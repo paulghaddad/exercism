04719c8b42960dee0103743f3efa34142b8c8e66
Paul Haddad <paulh16@gmail.com>
2018-10-24 17:04:36 -0400

    Python - Bob  * Iteration 1

---
+++ python/bob/bob.py
@@ -0,0 +1,19 @@
+import re
+
+
+def hey(phrase):
+    condensed_phrase = re.sub(r"\s", "", phrase)
+
+    is_question = condensed_phrase.endswith("?")
+    is_uppercase = condensed_phrase.isupper()
+
+    if is_question and is_uppercase:
+        return "Calm down, I know what I'm doing!"
+    elif is_question:
+        return "Sure."
+    elif is_uppercase:
+        return "Whoa, chill out!"
+    elif not condensed_phrase:
+        return "Fine. Be that way!"
+    else:
+        return "Whatever."
