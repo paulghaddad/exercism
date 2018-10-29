e30bae7a85d75e5a8bfbb742ac31776170dff6fb
Paul Haddad <paulh16@gmail.com>
2018-10-29 13:15:39 -0400

    Python - Leap
    * Use lambdas to reduce duplication.

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -2,7 +2,12 @@
 Exercism Problem 2: Leap
 """
 
+
 def is_leap_year(year):
     """Return True if the year is a leap year"""
+    mod = divider(year)
+    return mod(4) and not mod(100) or mod(400)
 
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+def divider(year):
+    return lambda d: year % d == 0
