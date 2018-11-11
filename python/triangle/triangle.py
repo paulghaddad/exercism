493c4c70f7c6d935f55f17e333332a1babd969a1
Paul Haddad <paulh16@gmail.com>
2018-11-11 10:07:40 -0500

    Python - Triangle
    * Iteration 2

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,19 +1,25 @@
-EQUILATERAL = 1
-ISOCELES = 2
-SCALENE = 3
+SIDES_FOR_TYPE = {"equilateral": 1, "isoceles": 2, "scalene": 3}
 
 
 def is_equilateral(sides):
-    return len(set(sides)) == EQUILATERAL and any(sides)
+    return is_kind("equilateral")(sides)
 
 
 def is_isosceles(sides):
-    return is_triangle(sides) and len(set(sides)) == ISOCELES or is_equilateral(sides)
+    return is_kind("isoceles")(sides) or is_equilateral(sides)
 
 
 def is_scalene(sides):
-    return is_triangle(sides) and len(set(sides)) == SCALENE
+    return is_kind("scalene")(sides)
 
 
-def is_triangle(sides):
-    return all(sides) and sum(sorted(sides)[:2]) > max(sides)
+def is_kind(type):
+    return lambda sides: _is_triangle(sides) and len(set(sides)) == SIDES_FOR_TYPE[type]
+
+
+def _is_triangle(sides):
+    return all(sides) and _valid_triangle(sides)
+
+
+def _valid_triangle(sides):
+    return sum(sorted(sides)[:2]) > max(sides)
