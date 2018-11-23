4dd69f0d19ea13e5223ebe373e3fba148a5e4da8
Paul Haddad <paulh16@gmail.com>
2018-11-23 12:54:31 -0500

    Python - Robot Name
    * Generate random names

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,20 @@
+import string
+import random
+
+
 class Robot(object):
     def __init__(self):
-        self.name = 'AA123'
+        self.name = self.create_random_name()
+
+
+    def create_random_name(self):
+        letters = string.ascii_uppercase
+
+        name = ''
+        for i in range(2):
+            name += random.choice(letters)
+
+        for i in range(3):
+            name += str(random.randint(0, 9))
+
+        return name
