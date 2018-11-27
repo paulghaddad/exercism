685f9a47c17da30f078717f87af6d2df81f4b6d0
Paul Haddad <paulh16@gmail.com>
2018-11-27 13:23:56 -0500

    Python - Space Age
    * Iteration 3
    * Dynamically set all planet period attributes

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -2,8 +2,7 @@ import re
 
 
 class SpaceAge(object):
-    EARTH_YEAR_IN_SECONDS = 31557600
-    ORBITAL_PERIODS = {
+    RELATIVE_ORBITAL_PERIODS = {
         "mercury": 0.2408467,
         "venus": 0.61519726,
         "earth": 1.0,
@@ -13,20 +12,12 @@ class SpaceAge(object):
         "uranus": 84.016846,
         "neptune": 164.79132,
     }
+    ORBITAL_PERIODS = [(planet, relative_earth_year * 31557600) for planet,
+                       relative_earth_year in RELATIVE_ORBITAL_PERIODS.items()]
+
 
     def __init__(self, seconds):
         self.seconds = seconds
-
-
-    def _relative_earth_years_for_planet(self, planet):
-        age = self.seconds / (
-            self.ORBITAL_PERIODS[planet] * self.EARTH_YEAR_IN_SECONDS
-        )
-        return round(age, 2)
-
-
-    def __getattr__(self, name):
-        planet_name = re.search(r'(?<=on_)\w+', name).group(0)
-        planet_func = lambda name=planet_name: self._relative_earth_years_for_planet(name)
-        setattr(self, name, planet_func)
-        return getattr(self, name)
+        for planet, period in self.ORBITAL_PERIODS:
+            period_calculation = lambda period=period: round(seconds / period, 2)
+            setattr(self, f"on_{planet}", period_calculation)
