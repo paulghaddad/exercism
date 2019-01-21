67edc6cf977c30f9c5552a2e728d6d90a340cc59
Paul Haddad <paulh16@gmail.com>
2019-01-21 15:24:38 -0500

    Python - Pangram
    * Iteration 2: Use a named import for the lowercase characters

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,8 +1,8 @@
-import string
+from string import ascii_lowercase as lowercase_letters
 
 
 def is_pangram(sentence):
-    chars_in_sentence = [char for char in sentence.lower() if char in string.ascii_lowercase]
+    chars_in_sentence = [char for char in sentence.lower() if char in lowercase_letters]
     letters_in_sentence = ''.join(chars_in_sentence)
 
-    return set(letters_in_sentence) == set(string.ascii_lowercase)
+    return set(letters_in_sentence) == set(lowercase_letters)
