05e3ca3286fc543775f0e9aef12b6a1a6422467d
Paul Haddad <paulh16@gmail.com>
2019-01-23 17:19:44 -0500

    Python - Pangram
    * Iteration 3: Use set.issubset

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,7 +2,6 @@ from string import ascii_lowercase as lowercase_letters
 
 
 def is_pangram(sentence):
-    chars_in_sentence = [char for char in sentence.lower() if char in lowercase_letters]
-    letters_in_sentence = ''.join(chars_in_sentence)
+    letters_in_sentence = set(char for char in sentence.lower() if char in lowercase_letters)
 
-    return set(letters_in_sentence) == set(lowercase_letters)
+    return set(lowercase_letters).issubset(letters_in_sentence)
