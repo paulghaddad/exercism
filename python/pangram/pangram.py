1cc2a48c28d93d8e5d052783b2c836e2632c1bd8
Paul Haddad <paulh16@gmail.com>
2019-01-25 17:13:52 -0500

    Python - Pangram
    * Final iteration; use both `issuperset` and `issubset`.

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,4 +2,8 @@ from string import ascii_lowercase as lowercase_letters
 
 
 def is_pangram(sentence):
+    # Approach 1: Are all the letters from a-z in the sentence
     return set(lowercase_letters).issubset(sentence.lower())
+
+    # Approach 2: Is every letter in a-z in the sentence
+    return set(sentence.lower()).issuperset(lowercase_letters)
