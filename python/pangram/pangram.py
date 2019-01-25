421e73fbda36a169c1bbb62d46ae0ecda122b21b
Paul Haddad <paulh16@gmail.com>
2019-01-25 16:46:06 -0500

    Python - Pangram
    * No need to do a set comprehension; just pass in sentence.lower()

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,6 +2,4 @@ from string import ascii_lowercase as lowercase_letters
 
 
 def is_pangram(sentence):
-    letters_in_sentence = set(char for char in sentence.lower())
-
-    return set(lowercase_letters).issubset(letters_in_sentence)
+    return set(lowercase_letters).issubset(sentence.lower())
