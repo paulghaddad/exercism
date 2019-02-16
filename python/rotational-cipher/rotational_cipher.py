113ea2e9c1c90ae7781a2b82a389795f0e3580d4
Paul Haddad <paulh16@gmail.com>
2019-02-16 07:25:50 -0500

    Python - Rotational Cipher
    * Add a second approach by passing in a mapping to maketrans.
    * Create this mapping using zip.

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -5,5 +5,14 @@ def rotate(text, key):
     rotated_lower = lower[key:] + lower[:key]
     rotated_upper = upper[key:] + upper[:key]
 
-    rotation_mapping = str.maketrans(lower+upper, rotated_lower + rotated_upper)
+    # Approach 1: pass in two arguments to maketrans and allow the function to
+    # create the mapping
+    rotation_mapping = str.maketrans(lower+upper, rotated_lower+rotated_upper)
     return text.translate(rotation_mapping)
+
+    # Approach 2: Create a dict of the mappings using zip and pass that in to
+    # maketrans as a single argument
+    rotated_char_mapping = dict(zip(lower+upper, rotated_lower+rotated_upper))
+    rotation_translation = str.maketrans(rotated_char_mapping)
+
+    return text.translate(rotation_translation)
