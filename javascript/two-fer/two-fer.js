ce379a81b4a9ca3c717552953419c94b8f035ff3
Paul Haddad <paulh16@gmail.com>
2019-03-16 08:05:54 -0400

    JS - Two-fer - Iteration 2

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,9 +1,5 @@
 function twoFer(name) {
-  if (name) {
-    return `One for ${name}, one for me.`;
-  }
-
-  return 'One for you, one for me.';
+  return `One for ${name || 'you'}, one for me.`;
 }
 
 export { twoFer };
