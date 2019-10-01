f8ca5b8f2832cf12acdef79f429f3ffbe89ddce4
Paul Haddad <paulh16@gmail.com>
2019-10-01 17:56:22 -0400

    Python - Raindrops * Iteration 2

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,20 +1,9 @@
-FACTORS_TO_SOUNDS = {
-    3: "Pling",
-    5: "Plang",
-    7: "Plong",
-}
+FACTORS_TO_SOUNDS = {3: "Pling", 5: "Plang", 7: "Plong"}
 
 
-def convert(number):
-    output = ""
+def convert(num):
+    return f"{sound(num, 3)}{sound(num, 5)}{sound(num, 7)}" or str(num)
 
-    if number % 3 == 0:
-        output += FACTORS_TO_SOUNDS[3]
 
-    if number % 5 == 0:
-        output += FACTORS_TO_SOUNDS[5]
-
-    if number % 7 == 0:
-        output += FACTORS_TO_SOUNDS[7]
-
-    return output or str(number)
+def sound(num, factor):
+    return FACTORS_TO_SOUNDS[factor] if num % factor == 0 else ""
