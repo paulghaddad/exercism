d3f16ea510ddead39110f7b01842fcd5b1aa06d8
Paul Haddad <paulh16@gmail.com>
2019-10-25 09:14:57 -0400

    Python - Hamming * Iteration 2

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,12 +1,8 @@
+from functools import reduce
+
+
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("The strands are not the same length.")
 
-    difference = 0
-
-    for position, letter_1 in enumerate(strand_a):
-        if letter_1 != strand_b[position]:
-            difference += 1
-
-    return difference
-
+    return reduce(lambda p, c: p + 1 if c[0] != c[1] else p, zip(strand_a, strand_b), 0)
